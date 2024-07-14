ff5e77f502f7a08b79ff5e5a683e08779b83e2e9
Geonwoo Kim <geonwkim@c1r7s7.42tokyo.jp>
2024-07-14 20:48:10 +0900

    [Update] - Norm error solved

--- playground/readline/readline.c
+++ playground/readline/readline.c
@@ -1,9 +1,21 @@
-#include "readline.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   readline.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: geonwkim <geonwkim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/14 20:45:25 by geonwkim          #+#    #+#             */
+/*   Updated: 2024/07/14 20:45:29 by geonwkim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include	"readline.h"
 
 int	main(void)
 {
 	char	*line;
-	int status;
+	int		status;
 
 	rl_outstream = stderr;
 	while (1)
@@ -13,9 +25,8 @@ int	main(void)
 			break ;
 		if (*line)
 			add_history(line);
-		status=ft_mlt_process(line);
+		status = ft_mlt_process(line);
 		free(line);
 	}
-	
 	exit(0);
 }
