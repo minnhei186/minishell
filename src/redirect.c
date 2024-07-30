351786ddc2cd4dbf09f208aaae7ab90387a3a294
Geonwoo Kim <geonwkim@c2r8s3.42tokyo.jp>
2024-07-30 16:01:13 +0900

    Update

---
+++ src/redirect.c
@@ -0,0 +1,65 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   redirect.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: geonwkim <geonwkim@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/07/30 15:54:31 by geonwkim          #+#    #+#             */
+/*   Updated: 2024/07/30 15:54:40 by geonwkim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <fcntl.h>
+#include <unistd.h>
+#include "readline.h"
+
+int	stashfd(int fd)
+{
+	int	stashfd;
+
+	stashfd = fcntl(fd, F_DUPFD, 10);
+	if (stashfd < 0)
+		fatal_error("fcntl");
+	if (close(fd) < 0)
+		fatal_error("close");
+	return (stashfd);
+}
+
+void	open_redir_file(t_node *redir)
+{
+	if (redir == NULL)
+		return ;
+	if (redir->kind == ND_REDIR_OUT)
+		redir->filefd = open(redir->filename->word, O_CREAT | O_WRONLY | O_TRUNC, 0644);
+	else
+		todo("open_redir_file");
+	redir->filefd = stashfd(redir->filefd);
+	open_redir_file(redir->next);
+}
+
+void	do_redirect(t_node *redir)
+{
+	if (redir == NULL)
+		return ;
+	if (redir->kind == ND_REDIR_OUT)
+	{
+		redir->stashed_targetfd = stashfd(redir->targetfd);
+		dup2(redir->filefd, redir->targetfd);
+	}
+	do_redirect(redir->next);
+}
+
+// Reset must be done from tail to head
+void	reset_redirect(t_node *redir)
+{
+	if (redir == NULL)
+		return ;
+	reset_redirect(redir->next);
+	if (redir->kind == ND_REDIR_OUT)
+	{
+		close(redir->filefd);
+		close(redir->targetfd);
+		dup2(redir->stashed_targetfd, redir->targetfd);
+	}
+}
