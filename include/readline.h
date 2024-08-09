a72e4aed5e87fb8661aa22e5e53d9d5305fa35f4
Geonwoo Kim <geonwkim@c2r4s4.42tokyo.jp>
2024-08-09 22:40:53 +0900

    [Update] - Sig

--- include/readline.h
+++ include/readline.h
@@ -6,7 +6,7 @@
 /*   By: geonwkim <geonwkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/17 21:04:25 by geonwkim          #+#    #+#             */
-/*   Updated: 2024/08/09 22:18:05 by geonwkim         ###   ########.fr       */
+/*   Updated: 2024/08/09 22:39:33 by geonwkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -97,8 +97,9 @@ struct						s_node
 // Environment
 extern int						last_status;
 extern char						**environ;
-extern bool						readline_interrupted;
-extern volatile sig_atomic_t	sig;
+extern bool						g_readline_interrupted;
+extern volatile sig_atomic_t	g_sig;
+
 
 /* Read Pipe */
 int		exec(t_node *node);
