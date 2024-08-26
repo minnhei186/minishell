2582b8c9824b119df81a10f5c0e3def96bdbe853
Geonwoo Kim <geonwkim@c3r6s3.42tokyo.jp>
2024-08-26 21:45:34 +0900

    test

--- libft/ft_strcat.c
+++ libft/ft_strcat.c
@@ -6,12 +6,15 @@
 /*   By: geonwkim <geonwkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 21:37:29 by geonwkim          #+#    #+#             */
-/*   Updated: 2024/08/26 21:43:40 by geonwkim         ###   ########.fr       */
+/*   Updated: 2024/08/26 21:45:26 by geonwkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"libft.h"
 
+// The 3rd argument `ft_strlcat` in `ft_strcat` is set to the combined
+// length of `dest` and `src` plus one for the null-terminator,
+// ensuring that all of `src` can be concatenated to `dest`
 char	*ft_strcat(char *dest, const char *src)
 {
 	ft_strlcat(dest, src, ft_strlen(dest) + ft_strlen(src) + 1);
