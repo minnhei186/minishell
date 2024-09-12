ae27ece57509d488e1fafd4a43abfdddcfee5650
Geonwoo Kim <geonwkim@c3r6s5.42tokyo.jp>
2024-09-12 22:13:42 +0900

    [Fix] - Export aa+=aaa has been solved

--- src/builtin_export.c
+++ src/builtin_export.c
@@ -6,7 +6,7 @@
 /*   By: geonwkim <geonwkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/25 04:11:17 by geonwkim          #+#    #+#             */
-/*   Updated: 2024/09/10 22:50:32 by geonwkim         ###   ########.fr       */
+/*   Updated: 2024/09/12 22:12:02 by geonwkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -108,12 +108,101 @@ void	print_allenv(t_map *envmap)
 	free(env_names);
 }
 
+// int	builtin_export(char **argv, t_map *envmap)
+// {
+// 	size_t	i;
+// 	int		status;
+
+// 	if (argv[1] == NULL)
+// 	{
+// 		print_allenv(envmap);
+// 		return (0);
+// 	}
+// 	status = 0;
+// 	i = 1;
+// 	while (argv[i])
+// 	{
+// 		if (map_put(envmap, argv[i], true) < 0)
+// 		{
+// 			builtin_error("export", argv[i], "not a valid identifier");
+// 			status = 1;
+// 		}
+// 		i++;
+// 	}
+// 	return (status);
+// }
+
+int	handle_append(t_map *envmap, const char *arg)
+{
+	char	*var_name;
+	char	*new_value;
+	char	*current_value;
+	char	*concatenated_value;
+	char	*plus_equal_sign;
+	char	*test;
+	char	*test1;
+	char	*test2;
+
+	// Locate the "+=" in the argument to separate name and value
+	plus_equal_sign = ft_strstr(arg, "+=");
+	if (!plus_equal_sign)  // Ensure "+=" is present in the string
+		return (1);
+
+	// Extract the variable name (everything before "+=")
+	var_name = ft_substr(arg, 0, plus_equal_sign - arg);  
+	if (!var_name)  // Memory allocation failed
+		return (1);
+
+	// Extract the value (everything after "+=")
+	new_value = ft_strdup(plus_equal_sign + 2);  
+	if (!new_value) 
+	{
+		free(var_name);  // Free var_name since new_value allocation failed
+		return (1);
+	}
+
+	// Retrieve the current value of the variable, if it exists
+	current_value = map_get(envmap, var_name);
+	printf("var_name = %s\n", var_name);
+	printf("new_var = %s\n", new_value);
+	printf("current_var = %s\n", current_value);
+
+	if (current_value)
+	{
+		// Append the new value to the current value
+		concatenated_value = ft_strjoin(current_value, new_value);
+	}
+	else
+	{
+		// If the variable doesn't exist, just assign the new value
+		concatenated_value = ft_strdup(new_value);
+	}
+
+	// Free temporary values (new_value and var_name)
+	free(new_value);
+	// free(var_name);
+
+	test = ft_strdup(var_name);
+	test1 = ft_strjoin(test, "=");
+	test2 = ft_strjoin(test1, concatenated_value);
+	// Ensure successful memory allocation before storing the value
+	if (!concatenated_value || map_put(envmap, test2, true) < 0)
+	{
+		free(concatenated_value);  // Free concatenated_value in case of failure
+		return (1);
+	}
+
+	// Successfully stored the concatenated value
+	free(concatenated_value);
+	return (0);
+}
+
 int	builtin_export(char **argv, t_map *envmap)
 {
 	size_t	i;
 	int		status;
 
-	if (argv[1] == NULL)
+	if (!argv[1])
 	{
 		print_allenv(envmap);
 		return (0);
@@ -122,7 +211,9 @@ int	builtin_export(char **argv, t_map *envmap)
 	i = 1;
 	while (argv[i])
 	{
-		if (map_put(envmap, argv[i], true) < 0)
+		if (ft_strstr(argv[i], "+="))  // Handle appending using `+=`
+			status |= handle_append(envmap, argv[i]);
+		else if (map_put(envmap, argv[i], true) < 0)  // Regular export case
 		{
 			builtin_error("export", argv[i], "not a valid identifier");
 			status = 1;
--- src/map_putter.c
+++ src/map_putter.c
@@ -6,11 +6,26 @@
 /*   By: geonwkim <geonwkim@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/29 00:14:55 by geonwkim          #+#    #+#             */
-/*   Updated: 2024/08/29 16:49:33 by geonwkim         ###   ########.fr       */
+/*   Updated: 2024/09/12 18:42:15 by geonwkim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/hashmap.h"
+
+//flag 0:write 1:read 
+// #define WRITE 0
+// #define READ 1
+
+// int exit_status(int flag, int value)
+// {
+// 	static int stk;
+
+// 	if (flag == WRITE) 
+// 		stk = value;
+// 	else if (flag == READ)
+// 		return stk;
+// 	return 0;
+// }
 
 static void	get_name_value(const char *string, bool allow_empty_value, \
 	char **name, char **value)
